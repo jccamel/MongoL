bc81549e942dac0aa74cd916a4ceefbbffc0fe72
Juan Carlos <jccamel@hotmail.com>
2016-07-17 10:08:50 +0200

    Delete mongol.py

--- mongol.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-from ShodanClass import ShodanObject
-
-if __name__ == "__main__":
-    data_file = "data/mongodb_list.json"
-    s = ShodanObject(data_file)
-    s.print_data_banner()
